e70709b05bcd9027ba12dc9d481407f99e4c8873
jurmat <jurmat.bbs@gmail.com>
2019-05-15 20:31:38 +0300

    lesson 2

--- api/controllers/article_controller.js
+++ api/controllers/article_controller.js
@@ -1,19 +1,35 @@
 var mongoose = require('mongoose'),
   Article = mongoose.model('Articles');
 
-  exports.list_articles = function(req, res){
-    Article.find({}, function(err, articles){
-      if(err)
-        res.send(err)
-      res.json(articles)
-    })
-  }
+exports.list_articles = function(req, res){
+  Article.find({}, function(err, articles){
+    if(err)
+      res.send(err)
+    res.json(articles)
+  })
+}
+exports.show_article = function(req , res){
+  var id = req.params.id;
+  Article.findById(id, function(err, article){
+    if(err)
+      res.send(err)
 
-  exports.create_an_article = function(req, res){
-    let new_article = new Article(req.body);
-    new_article.save(function(err, article){
-      if(err)
-        res.send(err)
-      res.json(article)
-    })
-  }
+    if(article){
+      res.json(article)  
+    }else{
+      res.status(404).send('not found')
+    }
+    
+    
+    
+  })
+}
+
+exports.create_an_article = function(req, res){
+  let new_article = new Article(req.body);
+  new_article.save(function(err, article){
+    if(err)
+      res.send(err)
+    res.json(article)
+  })
+}
--- api/routes/articles_routes.js
+++ api/routes/articles_routes.js
@@ -4,4 +4,7 @@ module.exports = function(app){
   app.route('/articles')
     .get(controller.list_articles)
     .post(controller.create_an_article)
+    
+  app.route('/articles/:id')
+  	.get(controller.show_article)
 }
