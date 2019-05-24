bd4d22b65dbc17ae51c732691931143a7850e4ec
derunva <derun.va@gmail.com>
2019-05-24 19:19:27 +0300

    add list goods

--- api/controllers/goods_controller.js
+++ api/controllers/goods_controller.js
@@ -1,10 +1,23 @@
 var mongoose = require('mongoose'),
   Goods = mongoose.model('Goods');
+var multer  = require('multer')
+var path = require('path')
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'public/goods/img')
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + path.extname(file.originalname)) //Appending extension
+  }
+})
+
+var upload = multer({ storage: storage }).single('image');
+
 exports.list =  (req, res)=>{
 
   let limit = Number(req.query.limit) || 6;
   let page = Number(req.query.page) || 1;
-  page--;
   let options = {
     select: 'labels image descr price',
     limit: limit,
@@ -22,14 +35,32 @@ exports.list =  (req, res)=>{
   
 }
 exports.add = (req, res) => {
-  console.log(req.body)
-  var goods_item = new Goods(req.body)
-  goods_item.save((err, item)=>{
-    if (err) {
-      res.status(400).send(err)
+  
+  
+  upload(req, res, function (err) {
+    console.log(req.body, req.file)
+    if (err instanceof multer.MulterError) {
+      // A Multer error occurred when uploading.
+      res.status(500).send(err)
+    } else if (err) {
+      // An unknown error occurred when uploading.
+      res.status(500).send(err)
     }else{
-      res.json(item)
+      var goods_item = new Goods(req.body)
+      if(req.file){
+        goods_item.image = req.file.filename
+      }
+      goods_item.save((err, item)=>{
+        if (err) {
+          res.status(400).send(err)
+        }else{
+          res.json(item)
+        }
+
+      })
     }
 
+    
   })
+  
 }
